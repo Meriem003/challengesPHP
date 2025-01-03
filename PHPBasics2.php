3e80a664970a1068e473dc7b269aaa45f80c96ba
meryam <meryemsalhi590@gmail.com>
2025-01-03 12:14:04 +0100

    challenges 4

---
+++ PHPBasics2.php
@@ -0,0 +1,4 @@
+<?php
+$age = "21";
+echo "my age is ". $age ."ans";
+?>
---
+++ PHPBasisc3.php
@@ -0,0 +1,5 @@
+<?php
+$numberOne = 10;
+$numbertow = 20;
+$sum = $numberOne + $numbertow;
+echo"$numberOne + $numbertow = $sum";

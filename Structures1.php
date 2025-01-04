97aeb9b79086e3987c4dd62fadbd5db6000d366a
meryam <meryemsalhi590@gmail.com>
2025-01-04 19:05:02 +0100

    Structures

---
+++ Structures1.php
@@ -0,0 +1,8 @@
+<?php
+$num = 12;
+if($num>10){
+echo "$num"."<br>";
+echo "num > 10";
+}else{
+    echo"num < 10";
+}
---
+++ Structures2.php
@@ -0,0 +1,7 @@
+<?php 
+$Age = 20;
+if($Age>18){
+echo("adult");
+}else{
+echo("mineure");
+}

a9a9f633a83915c9466e17cdb74314fe029a20ec
meryam <meryemsalhi590@gmail.com>
2025-01-04 19:36:05 +0100

    Structures5

---
+++ Structures5.php
@@ -0,0 +1,28 @@
+<?php
+$day = "5";
+
+switch ($day) {
+      case 1:
+        echo "day = Sunday";
+        break;
+      case 2:
+        echo "day = Monday";
+        break;
+      case 3:
+        echo "day = Tuesday";
+        break;
+      case 4:
+        echo "day = Wednesday";
+        break;
+      case 5:
+        echo "day = Thursday";
+        break;
+      case 6:
+        echo "day = Friday";
+        break;
+      case 7:
+        echo "day = Saturday";
+  default:
+    echo "error";
+}
+?>
